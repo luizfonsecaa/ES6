bc5685684a63f5dca3d08e16374609f63f775557
LuizFonseca <luiz.otavio@take5.com.br>
2018-04-15 18:08:20 -0300

    Criação da classe lista negociação

--- client/js/app/controller/NegociacaoController.js
+++ client/js/app/controller/NegociacaoController.js
@@ -4,23 +4,39 @@ class  NegociacaoController{
         constructor(){
             let $ = document.querySelector.bind(document); // adicionando o $ no javascript puro, 'bind faz ele usar o metodo da classe querySelector' 
             // pegandod os valores do inpult
-            this._inpultData      = $("#data");     
-            this._inpulQuantidade = $("#quantidade");
-            this._inpulValor      = $("#valor");
+            this._inpultData       = $("#data");     
+            this._inpulQuantidade  = $("#quantidade");
+            this._inpulValor       = $("#valor");
+            this._listaNegociacoes = new ListaNegociacoes();
     
         }
-       
+    
+    // função com a ação do enviar na view
     adiciona(event){
+
         event.preventDefault(); 
-        //criando   a classe negociação
-        let negociacao = new Negociacao(
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaformulario();
+        console.log(this._listaNegociacoes.negociacoes);
+    }
+
+    //cria uma negociação
+    _criaNegociacao(){
+
+        return new Negociacao(
             DateHelper.textoParaData(this._inpultData.value),
             this._inpulQuantidade.value,
             this._inpulValor.value
         );
-
-        let dataFormatada = DateHelper.dataParaTexto(negociacao.data);
-        console.log(negociacao);  
-        console.log(dataFormatada);  
     }
+
+    //limpa o formulario e da o foco no campo data
+    _limpaformulario(){
+
+        this._inpultData.value = '';
+        this._inpulQuantidade.value = 1;
+        this._inpulValor.value = 0.0;
+        this._inpultData.focus();
+    }
+
 }
